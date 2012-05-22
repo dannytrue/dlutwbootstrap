89b50d9f56d977ed62731c933439792b072b644c
David Lukas <david.lukas@zfdaily.com>
2012-05-22 16:09:02 +0200

    Refactoring to newer ZF2 version

--- Module.php
+++ Module.php
@@ -3,18 +3,6 @@ namespace DluTwBootstrap;
 
 class Module
 {
-    /**
-     * View Renderer
-     * @var \Zend\View\Renderer\PhpRenderer
-     */
-    protected $renderer;
-
-    /**
-     * Navbar container
-     * @var \Zend\Navigation\Navigation
-     */
-    protected $navbarContainer;
-
     /* ********************** METHODS ************************** */
 
     public function getAutoloaderConfig() {
@@ -34,6 +22,10 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
 
+    public function init(\Zend\ModuleManager\ModuleManager $moduleManager) {
+        $sharedEvents   = $moduleManager->events()->getSharedManager();
+        $sharedEvents->attach(__NAMESPACE__, 'dispatch', array($this, 'onThisModuleDispatch'));
+    }
 
     /**
      * OnBootstrap listener
@@ -44,15 +36,10 @@ class Module
      * @link http://mwop.net/slides/2012-04-25-ViewWebinar/Zf2Views.html#slide41
      * @param $e
      */
-    public function onBootstrap(\Zend\EventManager\Event $e) {
-        $app      = $e->getParam('application');
-        /* @var $app \Zend\Mvc\Application */
+    public function onBootstrap(\Zend\Mvc\MvcEvent $e) {
+        $app            = $e->getApplication();
         $serviceManager = $app->getServiceManager();
-        //$locator  = $serviceManager->get('dependencyInjector');
-        /* @var $locator \Zend\Di\Di */
-        $renderer         = $serviceManager->get('viewRenderer');
-        //$this->navbarContainer  = $locator->get('dlutwb-nav-menu-main');
-
+        $renderer       = $serviceManager->get('viewRenderer');
         //Register DluTwBootstrap view navigation helpers
         $renderer->plugin('navigation')
                        ->getPluginLoader()
@@ -60,42 +47,48 @@ class Module
                        ->registerPlugin('twbNavList', 'DluTwBootstrap\View\Helper\Navigation\TwbNavList')
                        ->registerPlugin('twbTabs', 'DluTwBootstrap\View\Helper\Navigation\TwbTabs')
                        ->registerPlugin('twbButtons', 'DluTwBootstrap\View\Helper\Navigation\TwbButtons');
-
         //Prepare the \Zend\Navigation\Page\Mvc for use in the navigation view helper
-        //\Zend\Navigation\Page\Mvc::setDefaultUrlHelper($this->renderer->plugin('url'));
-
+        \Zend\Navigation\Page\Mvc::setDefaultUrlHelper($renderer->plugin('url'));
         //Attach a listener to the app route event
         $app->events()->attach('route', array($this, 'onRoute'));
     }
 
     /**
      * OnRoute listener
-     * - Configures the url view helper to be usable in navigation view helpers
-     * - Sets the layout for actions from this module
      * @param \Zend\Mvc\MvcEvent $e
      */
     public function onRoute(\Zend\Mvc\MvcEvent $e) {
-        $routeMatch      = $e->getRouteMatch();
-        /*
-        //Configure routeMatchInjector with the current routeMatch and get it
-        $this->locator->instanceManager()->setParameters(
+        $serviceManager = $e->getApplication()->getServiceManager();
+        $renderer       = $serviceManager->get('viewRenderer');
+        $locator        = $serviceManager->get('dependencyInjector');
+        /* @var $locator \Zend\Di\Di */
+        $routeMatch     = $e->getRouteMatch();
+        //Configure routeMatchInjector with the current routeMatch
+        $locator->instanceManager()->setParameters(
             'DluTwBootstrap\Navigation\RouteMatchInjector', array(
                 'routeMatch' => $routeMatch,
         ));
-        $routeMatchInjector = $this->locator->get('DluTwBootstrap\Navigation\RouteMatchInjector');
-        */
-        /* @var $routeMatchInjector \DluTwBootstrap\Navigation\RouteMatchInjector */
         //Inject routeMatch into url helper
-        //$this->renderer->plugin('url')->setRouteMatch($routeMatch);
-        //Module specific bootstrapping
-        $controller = $routeMatch->getParam('controller');
-        if (strpos($controller, __NAMESPACE__) === 0) {
-            //Set the layout template for every action in this module
-            $viewModel = $e->getViewModel();
-            $viewModel->setTemplate('layout/layouttwb-demo');
-            //$viewModel->setVariable('navbar', $this->navbarContainer);
-            //Inject routeMatch into every MVC page, otherwise marking pages as active does not work
-            //$routeMatchInjector->injectRouteMatch($this->navbarContainer);
-        }
+        $renderer->plugin('url')->setRouteMatch($routeMatch);
+    }
+
+    public function onThisModuleDispatch(\Zend\Mvc\MvcEvent $e) {
+
+        //Set the layout template for every action in this module
+        $controller         = $e->getTarget();
+        $controller->layout('layout/layouttwb-demo');
+
+        //Set the main menu into the layout view model
+        $serviceManager     = $e->getApplication()->getServiceManager();
+        $locator  = $serviceManager->get('dependencyInjector');
+        /* @var $locator \Zend\Di\Di */
+        $navbarContainer    = $locator->get('dlutwb-nav-menu-main');
+        $viewModel          = $e->getViewModel();
+        $viewModel->setVariable('navbar', $navbarContainer);
+
+        //Inject routeMatch into every MVC page, otherwise marking pages as active does not work
+        $routeMatchInjector = $locator->get('DluTwBootstrap\Navigation\RouteMatchInjector');
+        /* @var $routeMatchInjector \DluTwBootstrap\Navigation\RouteMatchInjector */
+        $routeMatchInjector->injectRouteMatch($navbarContainer);
     }
 }
--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,22 @@
 <?php
 return array(
+    'router' => array(
+        'routes' => array(
+            'twBootstrapDemo' => array(
+                'type'    => 'Zend\Mvc\Router\Http\Segment',
+                'options' => array(
+                    'route'    => '/tw-bootstrap-demo[/:action]',
+                    'constraints' => array(
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'tw-bootstrap-demo',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
     'controller' => array(
         'classes' => array(
             'tw-bootstrap-demo' => 'DluTwBootstrap\Controller\DemoController'
@@ -11,35 +28,13 @@ return array(
             'layout/layouttwb-demo' => __DIR__ . '/../view/layout/layouttwb-demo.phtml',
         ),
         'template_path_stack' => array(
-            'dluTwBootstrap'    => __DIR__ . '/../view',
-            'partials'          => __DIR__ . '/../view/partials',
+            'dluTwBootstrap'            => __DIR__ . '/../view',
+            'dluTwBootstrap-partials'   => __DIR__ . '/../view/partials',
         ),
     ),
 
-
     'di'    => array(
         'instance'  => array(
-            //Setup for router and routes
-            'Zend\Mvc\Router\RouteStack' => array(
-                'parameters' => array(
-                    'routes' => array(
-                        'twBootstrapDemo' => array(
-                            'type'    => 'Zend\Mvc\Router\Http\Segment',
-                            'options' => array(
-                                'route'    => '/tw-bootstrap-demo[/:action]',
-                                'constraints' => array(
-                                    'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                ),
-                                'defaults' => array(
-                                    'controller' => 'DluTwBootstrap\Controller\DemoController',
-                                    'action'     => 'index',
-                                ),
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-
             'alias'     => array(
                 'dlutwb-nav-menu-main'      => 'Zend\Navigation\Navigation',
             ),
@@ -49,7 +44,7 @@ return array(
                         array(
                             'label'         => 'Forms',
                             'module'        => 'DluTwBootstrap',
-                            'controller'    => 'DluTwBootstrap\Controller\DemoController',
+                            'controller'    => 'tw-bootstrap-demo',
                             'action'        => 'form'
                         ),
                         array(
@@ -59,25 +54,25 @@ return array(
                                 array(
                                     'label'         => 'Navbar (static)',
                                     'module'        => 'DluTwBootstrap',
-                                    'controller'    => 'DluTwBootstrap\Controller\DemoController',
+                                    'controller'    => 'tw-bootstrap-demo',
                                     'action'        => 'navbar',
                                 ),
                                 array(
                                     'label'         => 'Nav List',
                                     'module'        => 'DluTwBootstrap',
-                                    'controller'    => 'DluTwBootstrap\Controller\DemoController',
+                                    'controller'    => 'tw-bootstrap-demo',
                                     'action'        => 'nav-list',
                                 ),
                                 array(
                                     'label'         => 'Tabs and Pills',
                                     'module'        => 'DluTwBootstrap',
-                                    'controller'    => 'DluTwBootstrap\Controller\DemoController',
+                                    'controller'    => 'tw-bootstrap-demo',
                                     'action'        => 'nav-tabs',
                                 ),
                                 array(
                                     'label'         => 'Buttons',
                                     'module'        => 'DluTwBootstrap',
-                                    'controller'    => 'DluTwBootstrap\Controller\DemoController',
+                                    'controller'    => 'tw-bootstrap-demo',
                                     'action'        => 'buttons',
                                 ),
                             ),
