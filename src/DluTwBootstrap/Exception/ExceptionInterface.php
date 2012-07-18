e5b7b2845628ceee53290dd421346f3dee44f48c
David Lukas <david.lukas@zfdaily.com>
2012-07-18 18:43:52 +0200

    Pagination control view partial - Standard

---
+++ src/DluTwBootstrap/Exception/ExceptionInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace DluTwBootstrap\Exception;
+
+/**
+ * DluTwBootstrap Exception interface
+ */
+interface ExceptionInterface
+{}
---
+++ src/DluTwBootstrap/Exception/ParameterMissingException.php
@@ -0,0 +1,10 @@
+<?php
+namespace DluTwBootstrap\Exception;
+
+/**
+ * ParameterMissingException
+ */
+class ParameterMissingException extends \InvalidArgumentException implements ExceptionInterface
+{
+
+}
